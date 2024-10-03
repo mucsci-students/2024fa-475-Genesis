160bdeb85420c7655601794fea89b49aba6174dd
PythonBlackBelt <pythonblackbelt@gmail.com>
2024-10-03 00:38:07 -0400

    Added player animations

--- GroupGenesisGame/Assets/Scripts/TopDownMovement.cs
+++ GroupGenesisGame/Assets/Scripts/TopDownMovement.cs
@@ -1,28 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TopDownMovement : MonoBehaviour
 {
 
-    public float moveSpeed;
-    public Rigidbody2D rb2d;
+    [SerializeField] private float moveSpeed;
+    private Rigidbody2D rb2d;
     private Vector2 moveInput;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
+        rb2d.velocity = moveInput * moveSpeed;
+    }
 
+    public void Move(InputAction.CallbackContext context)
+    {
+        animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+
+        moveInput = context.ReadValue<Vector2>();
         moveInput.Normalize();
 
-        rb2d.velocity = moveInput * moveSpeed;
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+
+
+        
+
+       
+        
     }
 }
