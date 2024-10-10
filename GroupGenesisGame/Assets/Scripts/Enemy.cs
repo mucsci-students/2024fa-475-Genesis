3556145baa8f3b65c12fa31576aa468bbca3ce17
PythonBlackBelt <pythonblackbelt@gmail.com>
2024-10-10 09:24:20 -0400

    Polished Attacked a Bit

--- GroupGenesisGame/Assets/Scripts/Enemy.cs
+++ GroupGenesisGame/Assets/Scripts/Enemy.cs
@@ -5,7 +5,8 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    private GameObject target;
+    GameObject target;
+    Animator animator;
     NavMeshAgent agent;
 
     // Start is called before the first frame update
@@ -15,11 +16,34 @@ public class Enemy : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        animator = GetComponent<Animator>();
+        animator.SetBool("Idle", true);
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(target.transform.position);
+        //Vector3 move = agent.nextPosition;
+        Vector3 move = agent.velocity;
+        animator.SetBool("Walk", true);
+
+        float X = move.x == 0 ? 0 : (move.x / Mathf.Abs(move.x));
+        float Y = move.y == 0 ? 0 : (move.y / Mathf.Abs(move.y));
+
+        if (move.x != 0 && move.y != 0)
+        {
+            animator.SetBool("Walk", true);
+            animator.SetBool("Idle", false);
+            animator.SetFloat("X", X);
+            animator.SetFloat("Y", Y);
+        }
+        else
+        {
+
+            animator.SetBool("Walk", false);
+            animator.SetBool("Idle", true);
+        }
+
     }
 }
